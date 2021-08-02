1786ee209eb6af2e22399f41a8d4919767a81826
Ana Chacon <chacongarcia.a@gmail.com>
2021-08-02 18:49:15 +0200

    layout and login buttons creation

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <a className='button1' href='https://developer.mozilla.org/es/'>layout</a>
+      <a className='button2' href='https://developer.mozilla.org/es/'>login</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,11 @@
+import './Layout.css';
+
+function Layout() {
+  return (
+    <div>
+      <a className='button' href='https://developer.mozilla.org/es/'>Botón</a>
+    </div>
+  );
+}
+
+export default App;
