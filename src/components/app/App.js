cca895176f7a706845e84e872ad767c2ec10aa67
Ana Chacon <chacongarcia.a@gmail.com>
2021-08-03 22:34:49 +0200

    add sidenav

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Home from '../home/Home';
 import Layout from '../layout/Layout';
 import Login from '../login/Login';
-import Menu from '../menu/Menu';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Menu , Section1, Section2 } from '../menu/Menu';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 
@@ -18,6 +18,8 @@ function App() {
           <Route path='/' exact component={Home}/> 
           <Route path='/layout' exact component={Layout}/>
           <Route path='/layout/menu' component={Menu} />
+          <Route path='/layout/menu/section1' component={Section1} />
+          <Route path='/layout/menu/section2' component={Section2} />
           <Route path='/login' component={Login} />
         </Switch>
       </div>
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,12 +1,35 @@
 import './Menu.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function Menu() {
+
+export function Menu() {
     return (
-      <div>
-          Aquí va el menu
-      </div>
+        <div class='sidenav'>
+            <Link to='/layout/menu/section1'>
+                <a>Section 1</a>
+            </Link>
+            <Link to='/layout/menu/section2'>
+                <a>Section 2</a>
+            </Link>
+        </div>
     );
   }
+
+
+export function Section1() {
+    return (
+    <div>
+    <h2>Section 1</h2>
+    <a href='https://duckduckgo.com/?q=export+react&atb=v261-1&ia=web'>link</a>
+    </div>
+    )
+  }
   
-  export default Menu;
+export function Section2() {
+    return <h2>Section 2</h2>;
+}
+
+  
+  
+  
