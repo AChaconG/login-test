ab59f3062be7343edfad895c41db50b10b4a0e4c
Ana Chacon <chacongarcia.a@gmail.com>
2021-08-06 19:05:05 +0200

    add hard coded login

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Home from '../home/Home';
 import Layout from '../layout/Layout';
 import Login from '../login/Login';
@@ -11,12 +11,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
+  //const [user, setUser] = useState[null];
+  const userFromDatabase = {userEmail: 'e@g.com', userPassword: 'tutato'};
+  
   return (
     <Router>
       <div>
         <Switch>
           <Route path='/' exact component={Home}/> 
-          <Route path='/login' component={Login} />
+          <Route path='/login'>
+            <Login userFromDatabase = {userFromDatabase}/>
+          </Route>
           <Route path='/layout' exact  component={Layout}/>
           <Route path='/layout/menu/' component={Menu} />
         </Switch>
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,64 @@
 import './Login.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-function Login() {
+
+function Login(props) {  
+  const [user, setUser] = useState({userEmail: '', userPassword: ''})
+  const [details, setDetails] = useState({userEmail: '', userPassword: ''})
+  const [error, setError] = useState('')
+  
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setDetails((previousState) => ({
+      ...previousState,
+      [name]: value
+    }))
+  }
+
+  const submitHandler = e => {
+    if (props.userFromDatabase.userEmail == details.userEmail && props.userFromDatabase.userPassword == details.userPassword) {
+      setUser({ 
+        userEmail: details.userEmail,
+        userPassword: details.userPassword
+      })
+    } else {
+        setError('Details do not match')
+    }
+    e.preventDefault();
+  }
+
+  const handleLogout = () => {
+    setUser({userEmail:'', userPassword:''});
+    setError('');
+  }
+  
+  
   return (
     <div>
-        <h1>login formulario</h1>
+    {(user.userEmail === '') ? (
+      <form onSubmit={submitHandler}>
+        <div>
+          <h2>Login</h2>
+          {(error != '') ? (<div>{error}</div>) : ''}
+          <label htmlFor='email'>Email:</label>
+          <input type='email' id='email' value={details.userEmail} onChange={handleChange} name='userEmail'></input>
+          <br/>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' id='password' value={details.userPassword} onChange={handleChange} name='userPassword'></input>
+          <br/>
+          <input type='submit' value='Submit' id='submit'></input>
+        </div>
+      </form>
+      ) : (
+        <div>
+        < h2>Welcome</h2>
+        <button onClick={handleLogout}>Logout</button>
+        </div>
+      )
+    }
     </div>
   );
 }
 
 export default Login;
+
